a6c2497e16a48f58f36e386a3ba07796b481a37f
Mauricio Dantas <dantas.mauricio.ti@gmail.com>
2025-11-09 16:41:27 -0300

    feature: set up on local machine

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Base Infrastructure on Digital Ocean Using Pulumi
-Prove of Concept of Continuos Deploy with Pulumi via Digital Ocean
+Prove of Concept of Continuos Deploy with Pulumi via Digital Ocean. Pulumi.template.yaml file have instructions on how you should set your configuration file.
+
+# If you are new to Pulumi
+
+- **pulumi stack init staging** to initializa staging env. The same you use to control production environment. Or developer if you go 3 ways
+- **pulumi preview** to see what is going to be provisioned
+- **pulumi up** and you should get your infrastructure set 
 
 ## If you are new to poetry Pack Management
 
--- __main__.py
+++ __main__.py
@@ -1,20 +1,18 @@
 import pulumi
+from pulumi import Config
 from modules.network.vpc import create_vpc
-from modules.network.subnet import create_subnets
 from modules.security.firewall import create_firewall
 
 # Basic config values
 env = pulumi.get_stack()
-region = "nyc3"
+config = Config()
+region = config.require("region")
 
 # Create VPC
 vpc = create_vpc(env, region)
 
-# Create subnets (optional, DO VPC covers the region)
-subnets = create_subnets(vpc, env)
-
-# Create firewall
-firewall = create_firewall(env, vpc)
+# Create firewall (commented in case you need a pre-existing firewall, not really relevant right now)
+# firewall = create_firewall(env, vpc)
 
 # Export identifiers
 pulumi.export("vpc_id", vpc.id)
