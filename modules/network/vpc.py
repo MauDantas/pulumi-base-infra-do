5d61d0c853b027dac9e159e32f9b01f8c71aeca5
Mauricio Dantas <dantas.mauricio.ti@gmail.com>
2025-11-21 19:24:46 -0300

    fix: code that works on pipeline

--- modules/network/vpc.py
+++ modules/network/vpc.py
@@ -1,11 +1,16 @@
 import pulumi
+from pulumi import Config
 import pulumi_digitalocean as do
 
+
 def create_vpc(env: str, region: str, provider: do.Provider) -> do.Vpc:
+    cfg = Config("vpc")
+    ip_range = cfg.require("ip_range")
+
     return do.Vpc(
         f"{env}-vpc",
         region=region,
-        ip_range="10.0.99.0/16",
+        ip_range=ip_range,
         description=f"{env} base VPC",
         opts=pulumi.ResourceOptions(provider=provider)
     )
