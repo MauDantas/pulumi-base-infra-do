50dc26ecd8a8dbb3ffe2ad2591f10402e57e25ab
Mauricio Dantas <dantas.mauricio.ti@gmail.com>
2025-11-21 18:44:34 -0300

    fix: move out from default vpcs

--- modules/network/vpc.py
+++ modules/network/vpc.py
@@ -5,7 +5,7 @@ def create_vpc(env: str, region: str, provider: do.Provider) -> do.Vpc:
     return do.Vpc(
         f"{env}-vpc",
         region=region,
-        ip_range="10.0.0.0/16",
+        ip_range="10.0.99.0/16",
         description=f"{env} base VPC",
         opts=pulumi.ResourceOptions(provider=provider)
     )
