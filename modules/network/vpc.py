f54514d81706716a4306b8b39aa93581aea3038c
Mauricio Dantas <dantas.mauricio.ti@gmail.com>
2025-11-21 16:22:03 -0300

    fix: python provider config

--- modules/network/vpc.py
+++ modules/network/vpc.py
@@ -1,13 +1,11 @@
+import pulumi
 import pulumi_digitalocean as do
 
-def create_vpc(env: str, region: str) -> do.Vpc:
-    """
-    Creates a DigitalOcean VPC.
-    """
-    vpc = do.Vpc(
+def create_vpc(env: str, region: str, provider: do.Provider) -> do.Vpc:
+    return do.Vpc(
         f"{env}-vpc",
         region=region,
         ip_range="10.0.0.0/16",
-        description=f"{env} base VPC"
+        description=f"{env} base VPC",
+        opts=pulumi.ResourceOptions(provider=provider)
     )
-    return vpc
