cb656fc67c86713b58477b414fe7ef5688b73029
Mauricio Dantas <dantas.mauricio.ti@gmail.com>
2025-11-21 14:54:31 -0300

    fix: python provider config

--- __main__.py
+++ __main__.py
@@ -1,18 +1,20 @@
 import pulumi
 from pulumi import Config
+import pulumi_digitalocean as do
+
 from modules.network.vpc import create_vpc
 from modules.security.firewall import create_firewall
 
-# Basic config values
 env = pulumi.get_stack()
-config = Config()
-region = config.require("region")
 
-# Create VPC
-vpc = create_vpc(env, region)
+cfg = Config("pulumi-base-infra-do")
+region = cfg.require("region")
 
-# Create firewall (commented in case you need a pre-existing firewall, not really relevant right now)
-# firewall = create_firewall(env, vpc)
+do_cfg = Config("digitalocean")
+token = do_cfg.require("token")
 
-# Export identifiers
+provider = do.Provider("do-provider", token=token)
+
+vpc = create_vpc(env, region, provider)
+
 pulumi.export("vpc_id", vpc.id)
